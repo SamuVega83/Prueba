442decbba8625a6925d3865dd7951458cb359bb0
SamuVega83 <samuelvegavazquez@alumno.ieselrincon.es>
2022-04-10 13:43:02 +0100

    Segunda versin

--- Prueba/src/prueba/Prueba.java
+++ Prueba/src/prueba/Prueba.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package prueba;
-
+//Comentario de prueba
 /**
  *
  * @author svv83
@@ -15,6 +15,7 @@ public class Prueba {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Prueba de control de versiones");
     }
     
 }
